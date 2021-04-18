ef428c1d299194cdad42a1f1f57e966bd7ccf13b
giovanni <giovanni@aimusic.co.uk>
2021-04-18 13:58:54 +0100

    test(app): correct test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { App } from './components/app/App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('App', () => {
+  it('renders title', () => {
+    render(<App />)
+    const text = screen.getByText(/Books You Might Like/i)
+    expect(text).toBeInTheDocument()
+  })
 })
